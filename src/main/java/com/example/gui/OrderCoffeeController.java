3766a813b8a4e0235d8c4aabb720294f619a4945
AHN333 <100101126+AHN333@users.noreply.github.com>
2023-04-05 17:27:42 -0400

    Added combobox items

--- src/main/java/com/example/gui/OrderCoffeeController.java
+++ src/main/java/com/example/gui/OrderCoffeeController.java
@@ -1,8 +1,11 @@
 package com.example.gui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,8 +13,10 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class OrderCoffeeController {
+public class OrderCoffeeController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -25,13 +30,15 @@ public class OrderCoffeeController {
     private RadioButton grandeRB, shortRB, ventiRB, tallRB;
 
     @FXML
-    private ComboBox<?> quantityCombobox;
+    private ComboBox<Integer> quantityCombobox;
 
     @FXML
     private ToggleGroup sizeToggleGrp;
 
     @FXML
     private TextField subtotalTF;
+    private final Integer[] QUANTITY = {1,2,3,4};
+    private final ObservableList<Integer> QUANTITY_LIST= FXCollections.observableArrayList(QUANTITY);
 
     @FXML
     void onAddToOrderClicked(ActionEvent event) {
@@ -44,5 +51,10 @@ public class OrderCoffeeController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        quantityCombobox.setItems(QUANTITY_LIST);
     }
 }
--- src/main/java/com/example/gui/OrderDonutController.java
+++ src/main/java/com/example/gui/OrderDonutController.java
@@ -1,8 +1,12 @@
 package com.example.gui;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,8 +18,10 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class OrderDonutController {
+public class OrderDonutController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -25,7 +31,9 @@ public class OrderDonutController {
     private Button addToListButton, addToOrderButton, backButton;
 
     @FXML
-    private ComboBox<?> donutTypeCombobox, quantityCombobox;
+    private ComboBox<String> donutTypeCombobox;
+    @FXML
+    private ComboBox<Integer> quantityCombobox;
 
     @FXML
     private TextField donutSubtotalTF;
@@ -35,7 +43,10 @@ public class OrderDonutController {
 
     @FXML
     private Button removeFromListButton;
-
+    private final String[] DONUT_TYPES = {"Yeast Donut", "Cake Donut", "Donut Hole"};
+    private final ObservableList<String> DONUT_TYPE_LIST = FXCollections.observableArrayList(DONUT_TYPES);
+    private final Integer[] QUANTITY = {1,2,3,4,5,6,7,8,9,10,11,12};
+    private final ObservableList<Integer> QUANTITY_LIST = FXCollections.observableArrayList(QUANTITY);
     @FXML
     void onAddToListClicked(ActionEvent event) {
 
@@ -59,4 +70,9 @@ public class OrderDonutController {
         stage.show();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        donutTypeCombobox.setItems(DONUT_TYPE_LIST);
+        quantityCombobox.setItems(QUANTITY_LIST);
+    }
 }
