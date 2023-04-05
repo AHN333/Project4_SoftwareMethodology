c1660e95b85ed6a816886da39a7a6868bbdfc2a1
AHN333 <100101126+AHN333@users.noreply.github.com>
2023-04-05 10:39:20 -0400

    Main controller methods

--- src/main/java/com/example/gui/CafeApplication.java
+++ src/main/java/com/example/gui/CafeApplication.java
@@ -11,7 +11,7 @@ public class CafeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CafeApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 550);
         stage.setTitle("RU Cafe");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/gui/MainController.java
+++ src/main/java/com/example/gui/MainController.java
@@ -1,14 +1,38 @@
 package com.example.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 
 public class MainController {
     @FXML
-    private Label welcomeText;
+    private Label ruCafe;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView basketMenuButton, coffeeMenuButton, donutMenuButton, ordersMenuButton;
+
+    @FXML
+    void onBasketClick(ActionEvent event) {
+
     }
+
+    @FXML
+    void onCoffeeClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDonutClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onOrdersClick(ActionEvent event) {
+
+    }
+
 }
