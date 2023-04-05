e2acc328badf4ec4dc0dd17283126a1864d09b23
AHN333 <100101126+AHN333@users.noreply.github.com>
2023-04-05 16:43:39 -0400

    Linked controller to fxml

--- src/main/java/com/example/gui/MainController.java
+++ src/main/java/com/example/gui/MainController.java
@@ -2,10 +2,17 @@ package com.example.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class MainController {
@@ -15,9 +22,16 @@ public class MainController {
     @FXML
     private ImageView basketMenuButton, coffeeMenuButton, donutMenuButton, ordersMenuButton;
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
-    void onBasketClick(ActionEvent event) {
-
+    void onBasketClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("order-basket-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
