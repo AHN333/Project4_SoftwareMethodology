bb021f243758798b986c4212bc9de549a04df2f3
AHN333 <100101126+AHN333@users.noreply.github.com>
2023-04-05 11:35:32 -0400

    Barebones gui

--- src/main/java/com/example/gui/OrderDonutController.java
+++ src/main/java/com/example/gui/OrderDonutController.java
@@ -1,4 +1,41 @@
 package com.example.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class OrderDonutController {
+    @FXML
+    private Button addToListButton, addToOrderButton;
+
+    @FXML
+    private ComboBox<?> cboxDonutType, cboxQuantity;
+
+    @FXML
+    private TextField donutSubtotalTF;
+
+    @FXML
+    private ListView<?> listViewCart, listViewFlavors;
+
+    @FXML
+    private Button removeFromListButton;
+
+    @FXML
+    void onAddToListClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAddToOrderClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRemoveFromListClicked(ActionEvent event) {
+
+    }
+
 }
